63e801b6d9ad303b02c819b60d055cd30452b249
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-08 09:43:19 +0530

    Adding Vowel Project

---
+++ Vowel/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("Enter Character");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o'  || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine("is Vowel", +ch);
+
+            }
+            else
+            {
+                Console.WriteLine("is Consonant", +ch);
+            }
+            Console.ReadLine();
+        }
+    }
+}
